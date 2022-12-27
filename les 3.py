efb0b1d4f377e8c412727870a574b52313ab75ad
ToxicButt <karkohash@mail.ru>
2022-12-28 00:25:36 +0300

    lessens 23

--- les 3.py
+++ les 3.py
@@ -1,9 +1,11 @@
-prev = {}
-for i in range(0, int(input())):
-    prev[i] = {}
-    prev[i]['name'] = input({'name'})
-    prev[i]['email'] = input({'email'})
+#prev = {}
+#for i in range(0, int(input())):
+    #prev[i] = {}
+   # prev[i]['name'] = input({'name'})
+  #  prev[i]['email'] = input({'email'})
 
-    print(prev)
+ #   print(prev)
 
 
+slovar = {i:{"name": input({"name"}), "email": input({"email"})} for i in range(0, int(input()))}
+print(slovar)
